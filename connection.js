7df40ea17a84654f9be4b03104766cbf735b7d54
harshit6789 <harshit.rexweb@gmail.com>
2021-05-05 17:28:05 +0530

    Done the forgot and reset api with nodemailer

---
+++ connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const url = process.env.db;
+let connectionFile = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (!err) {
+        console.log("db is connected");
+    } else {
+        console.log("Some occur is occured" + err);
+    }
+})
+
+module.exports = connectionFile;
