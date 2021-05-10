db6beaf6031bb2f7a26f1df7b897d8714cbb4eb6
deepshikha <deepshikha.rexweb@gmail.com>
2021-05-10 12:55:17 +0530

    add update route

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const validate = require('../validate');
 const bodyParser = require('body-parser');
-const { register, logIn, forgotPassword, resetPassword, deleteUser, joinAstrology } = require('../controller/adminPanel');
+const { register, logIn, forgotPassword, resetPassword, deleteUser, joinAstrology , updateUser} = require('../controller/adminPanel');
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -24,4 +24,8 @@ router.post('/deleteUser/:id', deleteUser);
 
 //join astrology
 router.post('/joinAstrology', joinAstrology);
+
+//update user
+router.patch('/updateUser/:id' , updateUser);
+
 module.exports = router;
